b01ad81b85d40fed1f1dfa127c04c2a3c7509a26
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-10-05 17:11:02 +0200

    OK, it creates a log file, but it is to be empty

--- src/main/java/dk/kb/copapi/UncaughtException.java
+++ src/main/java/dk/kb/copapi/UncaughtException.java
@@ -7,13 +7,18 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 @Provider
 public class UncaughtException extends Throwable implements ExceptionMapper<Throwable>
 {
     private static final long serialVersionUID = 1L;
+    private static Logger logger = LogManager.getLogger(API.class);
 
-
-    public Response toResponse(Throwable exception) {
+    public Response toResponse(Throwable ex) {
+	logger.error(ex.toString(), ex);
         return Response.status(500).entity("Sorry, an error has occurred. are you sure that you have the correct parameters?").type("text/plain").build();
     }
 }
