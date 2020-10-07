3715a19aea2c6fc2fcd250d112f735296abf0d9d
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-10-07 10:03:16 +0200

    now we compiles

--- src/main/java/dk/kb/copapi/JspLogger.java
+++ src/main/java/dk/kb/copapi/JspLogger.java
@@ -1,23 +1,32 @@
+package dk.kb.copapi;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
  
 public class JspLogger
 {
     private static Logger logger = LogManager.getLogger(JspLogger.class);
- 
-    public void debug(String jsp, String msg) {        
+
+    public class RunPull {}
+    
+    public void debug(String jsp, String msg)
+    {        
         logger.debug("JSP {} reports {}",jsp,msg);
     }
 
-    public void info(String jsp, String msg) {        
+    public void info(String jsp, String msg)
+    {        
         logger.info("JSP {} informs {}",jsp,msg);
     }
 
-    public void error(String jsp, String msg) {        
+    public void error(String jsp, String msg)
+    {        
         logger.error("JSP {} report error {}",jsp,msg);
     }
 
-    public void warn(String jsp, String msg) {        
+    public void warn(String jsp, String msg)
+    {        
         logger.warn("JSP {} warns {}",jsp,msg);
     }
 }
+
