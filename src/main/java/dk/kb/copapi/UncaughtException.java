12d7b7b4341b0164fcf2ea7e1c87943308b3243a
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-10-07 09:54:38 +0200

    Not trying to log in JSP

--- src/main/java/dk/kb/copapi/UncaughtException.java
+++ src/main/java/dk/kb/copapi/UncaughtException.java
@@ -18,7 +18,7 @@ public class UncaughtException extends Throwable implements ExceptionMapper<Thro
     private static Logger logger = LogManager.getLogger(API.class);
 
     public Response toResponse(Throwable ex) {
-	logger.error(ex.toString(), ex);
+	logger.error("A problem with a request {} or {}", ex.toString(), ex);
         return Response.status(500).entity("Sorry, an error has occurred. are you sure that you have the correct parameters?").type("text/plain").build();
     }
 }
