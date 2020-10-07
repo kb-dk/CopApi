7f94b02fd3d0066dabfdcf7a3c63fa80c832692f
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-10-07 12:08:35 +0200

    sort of works

--- src/main/java/dk/kb/copapi/URLReader.java
+++ src/main/java/dk/kb/copapi/URLReader.java
@@ -6,13 +6,16 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.net.URL;
 import java.net.URLConnection;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by laap on 03-05-2017.
  */
 public class URLReader {
 
+    private static Logger logger = LogManager.getLogger(URLReader.class);
+    
     public Document getDocument(String url) throws Exception {
 
         DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
@@ -21,11 +24,16 @@ public class URLReader {
         DocumentBuilder b = null;
         b = f.newDocumentBuilder();
         URLConnection urlConnection = new URL(url).openConnection();
+
+	String status = null; //urlConnection.getHeaderField("Status");
+	if(status != null ) {
+	    logger.info("Get {} for URI = {}",status,url);
+	}
+	
         urlConnection.addRequestProperty("Accept", "application/xml");
         return b.parse(urlConnection.getInputStream());
     }
 }
 
 
-
-
+    
